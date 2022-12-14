28768f85b827155ae95706aa116720083cf2e0c5
Zhukov Nikita <nypno@mail.ru>
2022-12-14 17:19:22 +0300

    change phone book

--- lesson7/search_abonent.py
+++ lesson7/search_abonent.py
@@ -1,5 +1,7 @@
 def search(word):
+    ab = []
     with open("hw/lesson7/log.txt", encoding='utf-8', mode='r') as pb:
         for line_no, line in enumerate(pb):
             if word in line:
-                return (line)
+                ab.append(line)
+        return (ab)
