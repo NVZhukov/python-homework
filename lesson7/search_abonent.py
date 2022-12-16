00c20315c90ae61a0162c254372e1c784a787fd9
Zhukov Nikita <nypno@mail.ru>
2022-12-16 17:45:27 +0300

    changed search abonent method

--- lesson7/search_abonent.py
+++ lesson7/search_abonent.py
@@ -1,7 +1,11 @@
 def search(word):
     ab = []
+    flag = True
     with open("hw/lesson7/log.txt", encoding='utf-8', mode='r') as pb:
         for line_no, line in enumerate(pb):
             if word in line:
+                flag = False
                 ab.append(line)
+        if flag:
+            ab.append(f'Контакт {word} не найден!')
         return (ab)
